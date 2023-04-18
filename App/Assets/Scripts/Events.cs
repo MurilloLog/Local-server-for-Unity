4a41104afdbeb30597184f238fa8a7eca75ced30
Gustavo Murillo <ga.murillogutierrez@ugto.mx>
2023-04-18 14:46:18 -0600

    upgrade App scene module

--- App/Assets/Scripts/Events.cs
+++ App/Assets/Scripts/Events.cs
@@ -15,40 +15,21 @@ using System.Text;
 public class Events : MonoBehaviour
 {
     public Networking networkBehaviour;
-    //public ConectionStatus conectionStatus;
     
     public string id = "";  // _id assigned by the server
     public bool readingFromServer = false;
     public bool writingToServer = false;
-    //public bool searchingRoom = false;
-    //public bool searchingPlayer = false;
-    //public bool paired = false;
     public bool error = false;
-    //public bool updatingPlayerPose = false;
-    //public bool updatingObjectPose = false;
-    //public bool targetIsFound = false;
-
-    //public SearchRoom searchRoom = new SearchRoom();
-    //public PlayerPose playerPose = new PlayerPose();
-    //public ObjectPose objectPose = new ObjectPose();
-    //public ObjectPose objectPose;
-    //public GameObject playerFrame;
-    //public GameObject objectPrefab;
-
+    
     public string JSONPackage = "";
     public JsonData JSONPackageReceived = new JsonData();
 
     void Awake()
     {
         networkBehaviour = FindObjectOfType<Networking>();
-        //conectionStatus = FindObjectOfType<ConectionStatus>();
         networkBehaviour.IP = "127.0.0.1"; // Server IP address
         networkBehaviour.PORT = 8080; // Server port
-        //conectionStatus.playerIsAlone = false;
-        //conectionStatus.playerIsWaiting = true;
     }
-
-    //public void changeTargetStatus() { targetIsFound = !targetIsFound; }
 
     // Receive a command from server and do ...
     public void readAction(string JsonFromServer)
@@ -59,12 +40,6 @@ public class Events : MonoBehaviour
             id = JsonFromServer.Replace("id: ", "");
             Debug.Log("Player ID from server received");
             Debug.Log("My player ID is: " + id);
-            //searchingRoom = true;
-            //searchRoom.setCommand("SEARCH_ROOM");
-            //searchRoom.setPlayerID(id);
-            //JSONPackage = JsonUtility.ToJson(searchRoom, true);
-            //Debug.Log("The message to send is: " + JSONPackage);
-            //sendRoomAction(JSONPackage);
         }
         else
         {
@@ -73,32 +48,19 @@ public class Events : MonoBehaviour
             switch (JSONPackageReceived.getCommand())
             {
                 case "MESSAGE_FROM_SERVER_1":
-                    //searchingPlayer = true;
-                    //paired = false;
-                    //conectionStatus.playerIsWaiting = true;
                     Debug.Log("I've received the message 1...");
                 break;
 
                 case "MESSAGE_FROM_SERVER_2":
-                    //paired = true;
-                    //searchingPlayer = false;
-                    //searchingRoom = false;
-                    //conectionStatus.playerIsWaiting = false;
                     Debug.Log("I've received the message 2...");
                 break;
 
                 case "MESSAGE_FROM_SERVER_3":
-                    //Debug.Log("Posicion: " + JSONPackageReceived.getPosition());
-                    //Debug.Log("Rotacion: " + JSONPackageReceived.getRotation());
-                    //updatingPlayerPose = true;
                     Debug.Log("I've received the message 3...");
                 break;
 
                 case "MESSAGE_FROM_SERVER_N":
-                    //updatingObjectPose = true;
-                    //UpdateEnvironment();
                     Debug.Log("I've received the message N...");
-                    //updatingObjectPose = false;
                 break;
 
                 default:
@@ -122,7 +84,7 @@ public class Events : MonoBehaviour
             return;
         try
         {
-            if(paired && !error)
+            if(!error)
             {
                 writingToServer = true;
                 networkBehaviour.stream.BeginWrite(Encoding.UTF8.GetBytes(sendJson), 0, sendJson.Length, new AsyncCallback(endWritingProcess), networkBehaviour.stream);
@@ -151,14 +113,15 @@ public class Events : MonoBehaviour
                 readingFromServer = true;
                 networkBehaviour.stream.BeginRead(networkBehaviour.data, 0, networkBehaviour.data.Length, new AsyncCallback(endReadingProcess), networkBehaviour.stream);
             }
-            /*else
-            {
-                if (paired && targetIsFound)
-                {
-                    UpdatePlayerPose();
-                    //UpdateEnvironment();
-                }
-            }*/
+
+            /*
+                ...
+                    Write your own functions
+                ...
+
+                JSONPackageObject = JsonUtility.ToJson(object, true);
+                sendRoomAction(JSONPackageObject);
+            */
         }
     }
 
@@ -173,74 +136,5 @@ public class Events : MonoBehaviour
     private void OnApplicationQuit()
     {
         networkBehaviour.isRunning = false;
-    }
-
-    private void UpdatePlayerPose() 
-    {
-        playerPose.poseUpdate();
-        if(playerPose.getPreviousMovement() != playerPose.getCurrentMovement())
-        {
-            playerPose.setPreviousMovement();
-            playerPose.setCommand("UPDATE_PLAYER_POSE");
-            playerPose.setPlayerID(id);
-            JSONPackage = JsonUtility.ToJson(playerPose, true);
-            sendAction(JSONPackage);
-            if(playerPose.isFirstPose())
-            {
-                playerFrame.name = "playerFrame";
-                playerFrame = (GameObject) Instantiate(playerFrame);
-                playerPose.setFirstPose();
-            }
-        }
-    }
-
-    private void UpdateEnvironment()
-    {
-        // Searching for a GameObject
-        //GameObject currentObject = new GameObject();
-        //float smooth = 10.0f;
-        //currentObject = GameObject.Find(JSONPackageReceived.getID());
-
-        if( Lean.Common.LeanSpawn.objects.ContainsKey(JSONPackageReceived.getID()) )
-        {
-            /*GameObject obj = Lean.Common.LeanSpawn.objects[JSONPackageReceived.getID()];
-            obj.transform.position = Vector3.Slerp(obj.transform.position, JSONPackageReceived.getPosition(), Time.deltaTime * smooth);
-            obj.transform.rotation = Quaternion.Slerp(obj.transform.rotation, JSONPackageReceived.getRotation(),  Time.deltaTime * smooth);
-            */Debug.Log("There is a GameObject with this ID");
-        }
-        else
-        {
-            Debug.Log("There is not a GameObject with this ID");
-            //var clone = Instantiate(objectPrefab, JSONPackageReceived.getPosition(), JSONPackageReceived.getRotation());
-            //objectPrefab = (GameObject) Instantiate(objectPrefab);//, JSONPackageReceived.getPosition(), JSONPackageReceived.getRotation());
-            //clone.gameObject.name = JSONPackageReceived.getID();
-            //objectPrefab.gameObject.SetActive(true);
-            objectPrefab = (GameObject) Instantiate(objectPrefab);//, JSONPackageReceived.getPosition(), JSONPackageReceived.getRotation())
-            Lean.Common.LeanSpawn.objects.Add(JSONPackageReceived.getID(), objectPrefab.gameObject);
-            Debug.Log("The new object was created");
-            //obj.AddComponent<UnityEngine.MeshRenderer>();
-            
-            //Lean.Common.LeanSpawn.objects.Add(JSONPackageReceived.getID(), obj.gameObject);*/
-            
-        }
-
-        /*if(currentObject == null)
-        {
-            currentObject.name = JSONPackageReceived.getID();
-            currentObject.AddComponent<UnityEngine.MeshRenderer>();
-            currentObject = (GameObject) Instantiate(currentObject);
-            
-            currentObject.transform.position = Vector3.Slerp(currentObject.transform.position, JSONPackageReceived.getPosition(), Time.deltaTime * smooth);
-            currentObject.transform.rotation = Quaternion.Slerp(currentObject.transform.rotation, JSONPackageReceived.getRotation(),  Time.deltaTime * smooth);
-            Debug.Log("Se creo el objeto del otro jugador.");
-        }
-        else
-        {
-            currentObject.transform.position = Vector3.Slerp(currentObject.transform.position, JSONPackageReceived.getPosition(), Time.deltaTime * smooth);
-            currentObject.transform.rotation = Quaternion.Slerp(currentObject.transform.rotation, JSONPackageReceived.getRotation(),  Time.deltaTime * smooth);
-            Debug.Log("Se actualizo el objeto del otro jugador.");
-        }*/
-        //JSONPackage = JsonUtility.ToJson(objectPose, true);
-        //sendAction(JSONPackage);
     }
 }
